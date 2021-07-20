3255bcf2bdb5c03cf8fe6282ab5acea186ade4a1
Juan Arredondo <juan.arredondo@gmail.com>
2021-07-19 22:31:15 -0400

    Installed PostgreSQL

--- myblog/settings.py
+++ myblog/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'juansblog',
+        'USER': 'juan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
