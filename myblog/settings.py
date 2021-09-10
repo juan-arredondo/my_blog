edcc8edf447350bdf1cab2ae108f3b79675ee544
Juan Arredondo <juan.arredondo@gmail.com>
2021-09-10 16:31:19 -0400

    Updated settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -110,12 +110,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'db.sqlite3')
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'db.sqlite3'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -12,4 +12,5 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
